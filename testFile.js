eaf014ccf38f3bad456cfba3caf15820d8660ce1
Ian Farrant <ianfarrant@gmail.com>
2023-06-06 12:38:38 -0400

    added wuj and her like for fashion

--- testFile.js
+++ testFile.js
@@ -4,7 +4,8 @@ let array = [
     {name: 'Ian', likes: 'Rock Climbing'},
     {name: 'Joe', likes: 'Playing Bass'},
     {name: 'Kevin', likes: 'Making Art'},
-    {name: 'Leon', likes: 'Graphic Design'}
+    {name: 'Leon', likes: 'Graphic Design'},
+    {name: 'Wuj', likes: 'Fashion'}
 ]
 
 for (let i = 0; i < array.length; i++) {
