4481c5fe2e099e21b42b2bcd26d595bf7edc93d1
Ian Farrant <ianfarrant@gmail.com>
2023-06-06 11:33:39 -0400

    created js file that loops through array and console logs data

---
+++ testFile.js
@@ -0,0 +1,11 @@
+// Practicing git
+
+let array = [
+    {name: 'Ian', likes: 'Rock Climbing'},
+    {name: 'Joe', likes: 'Playing Bass'},
+    {name: 'Kevin', likes: 'Making Art'}
+]
+
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i].name + " really likes " + array[i].likes);
+}
